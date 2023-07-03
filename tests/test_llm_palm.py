7099de27f306184bcb7909c44e4b99ba51644d39
Simon Willison <swillison@gmail.com>
2023-07-02 18:08:57 -0700

    Test for the 'llm palm models' command, refs #2

--- tests/test_llm_palm.py
+++ tests/test_llm_palm.py
@@ -1,5 +1,9 @@
+import ast
+from click.testing import CliRunner
 from unittest.mock import patch, Mock
+from llm.cli import cli
 from llm_palm import PalmResponse, Prompt
+import json
 
 
 @patch("llm_palm.palm")
@@ -14,3 +18,17 @@ def test_palm_response(mock_palm):
     mock_palm.chat.assert_called_with(messages="hello")
 
     assert items == ["hello"]
+
+
+@patch("llm_palm.palm")
+def test_palm_models(mock_palm):
+    fake_models = [
+        {"name": "'models/chat-bison-001"},
+        {"name": "models/text-bison-001"},
+    ]
+    # Mock palm.list_models()
+    mock_palm.list_models.return_value = fake_models
+    runner = CliRunner()
+    result = runner.invoke(cli, ["palm", "models"])
+    assert result.exit_code == 0
+    assert ast.literal_eval(result.output) == fake_models
